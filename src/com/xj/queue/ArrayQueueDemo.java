897bb4b985b08b2d1a390bf3c03e60787d14751f
yu <1395759132@qq.com>
2020-01-09 22:30:23 +0800

    commit day 2

---
+++ src/com/xj/queue/ArrayQueueDemo.java
@@ -0,0 +1,56 @@
+package com.xj.queue;
+
+public class ArrayQueueDemo {
+    public static void main(String[] args) {
+        ArrayQueue arrayQueue = new ArrayQueue(3);
+        arrayQueue.addQueue(1);
+        arrayQueue.addQueue(2);
+        arrayQueue.addQueue(3);
+        arrayQueue.addQueue(4);
+    }
+}
+class ArrayQueue{
+    private int front;//队头
+    private int rear;//队尾
+    private int maxSize;//数组最大容量
+    private int[] arr;//存放数据模拟队列
+
+    public ArrayQueue(int maxSize){
+        this.maxSize = maxSize;
+        arr = new int[maxSize];
+        front = -1;
+        rear = -1;
+    }
+    //判断队列是否为空
+    public boolean isEmpty(){
+        return front == rear;
+    }
+    //判断队列是否满
+    public boolean isFull(){
+        return rear == maxSize - 1;
+    }
+    //出队
+    public int getQueue(){
+        if (isEmpty()){
+            throw new RuntimeException("队空，无法取出数据");
+        }
+        front++;//队头往后移动一位
+        return arr[front];
+    }
+    //入队
+    public void addQueue(int n){
+        if (isFull()){
+            System.err.println("队满,无法存储数据");
+            return;
+        }
+        rear++;//队尾往后移一位
+        arr[rear] = n;
+    }
+    //查看队头
+    public int getQueueHead(){
+        if (isEmpty()){
+            throw new RuntimeException("队空队空，无法取出数据");
+        }
+        return arr[front + 1];
+    }
+}
