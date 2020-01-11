0ab5fb8de00fd44e9c47b73d4ece209294221096
yu <1395759132@qq.com>
2020-01-11 20:05:31 +0800

    完成反转结点以及合并两个有序链表

--- src/com/xj/linkedlist/SingleLinkedListDemo.java
+++ src/com/xj/linkedlist/SingleLinkedListDemo.java
@@ -1,18 +1,34 @@
 package com.xj.linkedlist;
 
+import java.util.Stack;
+
 public class SingleLinkedListDemo {
     public static void main(String[] args) {
         HeroNode heroNode1 = new HeroNode(1, "宋江", "及时雨");
         HeroNode heroNode2 = new HeroNode(2, "吴用", "智多星");
         HeroNode heroNode3 = new HeroNode(3, "林冲", "豹子头");
         HeroNode heroNode4 = new HeroNode(4, "鲁智深", "忘记了");
-        SingleLinkedList linkedList = new SingleLinkedList();
-        linkedList.addOrderBy(heroNode1);
-        linkedList.addOrderBy(heroNode3);
-        linkedList.addOrderBy(heroNode2);
-        linkedList.addOrderBy(heroNode4);
-        System.out.println(SingleLinkedList.size(linkedList.getHeadNode()));
-        System.out.println(SingleLinkedList.findLastIndexNode(linkedList.getHeadNode(),3));
+        SingleLinkedList linkedList1 = new SingleLinkedList();
+        linkedList1.addOrderBy(heroNode1);
+        linkedList1.addOrderBy(heroNode2);
+        linkedList1.addOrderBy(heroNode3);
+        linkedList1.addOrderBy(heroNode4);
+        HeroNode Node1 = new HeroNode(45, "宋江", "及时雨");
+        HeroNode Node2 = new HeroNode(75, "吴用", "智多星");
+        HeroNode Node3 = new HeroNode(7, "林冲", "豹子头");
+        HeroNode Node4 = new HeroNode(8, "鲁智深", "忘记了");
+        SingleLinkedList linkedList2 = new SingleLinkedList();
+        linkedList2.addOrderBy(Node1);
+        linkedList2.addOrderBy(Node2);
+        linkedList2.addOrderBy(Node3);
+        linkedList2.addOrderBy(Node4);
+        SingleLinkedList singleLinkedList = SingleLinkedList.connectLinkedList(linkedList1, linkedList2);
+        HeroNode headNode = singleLinkedList.getHeadNode();
+        HeroNode cur = headNode.nextNode;
+        while (cur.nextNode != null) {
+            System.out.println(cur);
+            cur = cur.nextNode;
+        }
     }
     public static void change(int n){
         n = 3;
@@ -26,7 +42,76 @@ class SingleLinkedList{
     public HeroNode getHeadNode() {
         return headNode;
     }
-
+    //合并两个有序列表，并且合并之后依然有序
+    public static SingleLinkedList connectLinkedList(SingleLinkedList linkedList1,SingleLinkedList linkedList2){
+        HeroNode cur1 = linkedList1.getHeadNode().nextNode;
+        HeroNode cur2 = linkedList2.getHeadNode().nextNode;
+        SingleLinkedList linkedList = new SingleLinkedList();
+        HeroNode cur = linkedList.getHeadNode();
+        //记录当前节点的下一个结点地址
+        HeroNode temp = null;
+        while(cur1 != null && cur2 != null){
+            if (cur1.no <= cur2.no) {
+                temp = cur1.nextNode;
+                cur.nextNode = cur1;
+                cur.nextNode.nextNode = null;
+                cur1 = temp;
+            }else {
+                temp = cur2.nextNode;
+                cur.nextNode = cur2;
+                cur.nextNode.nextNode = null;
+                cur2 = temp;
+            }
+            cur = cur.nextNode;
+        }
+        while(cur1 != null){
+            temp = cur1.nextNode;
+            cur.nextNode = cur1;
+            cur = cur.nextNode;
+            cur1 = temp;
+        }
+        while(cur2 != null){
+            temp = cur2.nextNode;
+            cur.nextNode = cur2;
+            if(cur.nextNode != null){
+                cur.nextNode.nextNode = null;
+            }
+            cur = cur.nextNode;
+            cur2 = temp;
+        }
+        System.out.println(1);
+        return linkedList;
+    }
+    //反转结点
+    public static void reverseNode(HeroNode headNode){
+        //如果当前链表为空或只有一个结点无需反转
+        if(headNode.nextNode == null && headNode.nextNode.nextNode == null){
+            return ;
+        }
+        HeroNode cur = headNode.nextNode;
+        HeroNode next = null;
+        HeroNode reverseHead = new HeroNode(0,null,null);
+        while (cur != null) {
+            next = cur.nextNode;//暂时保存下一个结点的地址
+            cur.nextNode = reverseHead.nextNode;//插入到新链表的最前端
+            reverseHead.nextNode = cur;
+            //cur指针后移
+            cur = next;
+        }
+        headNode.nextNode = reverseHead.nextNode;
+    }
+    //逆序打印链表信息
+    public static void reversePrint(HeroNode headNode){
+        Stack<HeroNode> stack = new Stack<>();
+        HeroNode cur = headNode.nextNode;
+        while (cur != null){
+            stack.push(cur);
+            cur = cur.nextNode;
+        }
+        while (stack.size() > 0){
+            System.out.println(stack.pop());
+        }
+    }
     //有效结点个数
     public static int size(HeroNode headNode){
         int count = 0;
